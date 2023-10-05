154ba6237b80196e00ffbb96230dca3ec7505fb9
tqthang1995 <tqthang1995@gmail.com>
2023-10-05 14:55:19 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=postgres.ceagpzho9lh5.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=tqthang14-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=tqthang14-udacity
+export URL=http://localhost:8100
